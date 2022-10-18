24bb2cc784ff82f56a1895a75b3664ff17c9fbec
comlekahmetcan <@comlekahmetcan.norepyl.github.com>
2022-10-18 11:05:36 +0300

    Compostion Api - Computed kullanımı

--- 0-composition-api/src/App.vue
+++ 0-composition-api/src/App.vue
@@ -1,11 +1,12 @@
 <template>
   <h3>{{ title }}</h3>
   <input type="text" v-model="title" />
+  {{ titleLengthMessage }}
   <button @click="toggleIt">Toogle</button>
   <p v-if="show">Lorem ipsum.......................</p>
 </template>
 <script>
-import { ref } from "vue"; //reactive effect
+import { ref, computed } from "vue"; //reactive effect
 export default {
   // data() {
   //   return {
@@ -22,16 +23,28 @@ export default {
   setup() {
     //Datadaki reactivity oluşmadan önce çalışmak ve datayı ona göre ayarlamak
     const title = ref("Bu bir setup başlık");
-    const show = ref(false);//ref bize bir obje döner
+    const show = ref(false); //ref bize bir obje döner
     //console.log("show", show.value);
+
     const toggleIt = () => {
-      show.value = !show.value;//show değerine ulaşabilmek için objenin valuesine ulaşabilmemiz lazım
+      show.value = !show.value; //show değerine ulaşabilmek için objenin valuesine ulaşabilmemiz lazım
     };
     // function toogleIt(){}
+
+    //tek satırlık bir kod için tanımlama
+    const titleLengthMessage = computed(() => title.value.length + " adet karakter yazdınız");
+    //birden fazla satır kod için tanımlama
+    // const titleLengthMessage = computed(() => {
+    //   return title.value.length + " adet karakter yazdınız";
+    // });
+
+    //console.log("titleLengthMessage", titleLengthMessage.value);
+
     return {
       title,
       show,
       toggleIt,
+      titleLengthMessage,
     };
   },
 };
