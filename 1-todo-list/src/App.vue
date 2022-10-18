5bacc941bea48a81e5a33930f46de02098c0ad2e
comlekahmetcan <@comlekahmetcan.norepyl.github.com>
2022-10-18 12:53:39 +0300

    todoList component düzenleme

--- 1-todo-list/src/App.vue
+++ 1-todo-list/src/App.vue
@@ -3,7 +3,7 @@
     <div class="bg-gray-700 rounded-md shadow-md text-white w-1/3 mx-auto p-3">
       <h3 class="text-center text-2xl">TodoList</h3>
       <AddSection />
-      <TodoList />
+      <TodoList :todoList="todoList" />
     </div>
   </div>
 </template>
@@ -11,10 +11,15 @@
 <script>
 import AddSection from "./components/AddSection.vue";
 import TodoList from "./components/todoList.vue";
+import { ref } from "vue";
 export default {
   components: {
     AddSection,
     TodoList,
+  },
+  setup() {
+    const todoList = ref([]);
+    return { todoList };
   },
 };
 </script>
--- 1-todo-list/src/components/todoList.vue
+++ 1-todo-list/src/components/todoList.vue
@@ -1,6 +1,7 @@
 <template>
   <section class="mt-4">
     <TodoListItem />
+    <small>{{ todoLength }} adet Item vardır</small>
     <MySummary />
   </section>
 </template>
@@ -8,10 +9,18 @@
 <script>
 import TodoListItem from "./todoListItem.vue";
 import MySummary from "./MySummary.vue";
+import { computed } from "vue";
 export default {
   components: {
     TodoListItem,
     MySummary,
+  },
+  props: {
+    todoList: Array,
+  },
+  setup(props) {
+    const todoLength = computed(() => props.todoList.length);
+    return { todoLength };
   },
 };
 </script>
