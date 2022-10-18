f21040c1048b6cb34727053ca6a3acbe7ce31a3c
comlekahmetcan <@comlekahmetcan.norepyl.github.com>
2022-10-18 10:39:20 +0300

    Ref(reactive effect) kullanımı

--- 0-composition-api/src/App.vue
+++ 0-composition-api/src/App.vue
@@ -1,8 +1,11 @@
 <template>
   <h3>{{ title }}</h3>
   <input type="text" v-model="title" />
+  <button @click="show = !show">Toogle</button>
+  <p v-if="show">Lorem ipsum.......................</p>
 </template>
 <script>
+import { ref } from "vue"; //reactive effect
 export default {
   // data() {
   //   return {
@@ -18,10 +21,11 @@ export default {
   // },
   setup() {
     //Datadaki reactivity oluşmadan önce çalışmak ve datayı ona göre ayarlamak
-    const title = "Bu bir setup başlık";
-
+    const title = ref("Bu bir setup başlık");
+    const show = ref(false);
     return {
       title,
+      show,
     };
   },
 };
