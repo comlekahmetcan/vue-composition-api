629e63588229a15e5b0f9f051f86c74a6796e7a5
comlekahmetcan <@comlekahmetcan.norepyl.github.com>
2022-10-19 14:04:33 +0300

    defineProps ile prop bilgilerini almak

--- 10-0-script-setup/src/App.vue
+++ 10-0-script-setup/src/App.vue
@@ -2,8 +2,11 @@
   <h3>{{ title }}</h3>
   <input type="text" v-model="title" />
   <button @click="inc">{{ counter }}</button>
+  <hr />
+  <oddOrEven :counter="counter" /><!--ilk gönderme buradan counter için-->
 </template>
 <script setup>
+import oddOrEven from "./components/oddOrEven.vue";
 import { ref } from "vue";
 const title = ref("Başlık");
 const counter = ref(0);
---
+++ 10-0-script-setup/src/components/oddOrEven.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1>aa</h1>
+  <h3>{{ props.counter }}</h3>
+</template>
+<script setup>
+const props = defineProps({ counter: Number });
+</script>
