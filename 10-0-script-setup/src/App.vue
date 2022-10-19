0a028e3d09440992b9ee9f39e791fe86ca889c57
comlekahmetcan <@comlekahmetcan.norepyl.github.com>
2022-10-19 13:44:22 +0300

    Event okumak ve method oluşturmak

--- 10-0-script-setup/src/App.vue
+++ 10-0-script-setup/src/App.vue
@@ -1,8 +1,13 @@
 <template>
   <h3>{{ title }}</h3>
   <input type="text" v-model="title" />
+  <button @click="inc">{{ counter }}</button>
 </template>
 <script setup>
 import { ref } from "vue";
 const title = ref("Başlık");
+const counter = ref(0);
+const inc = () => {
+  counter.value++;
+};
 </script>
