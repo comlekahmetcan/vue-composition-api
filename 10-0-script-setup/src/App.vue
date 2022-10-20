c6169eaed47ee2f10cbd9d4f80c2325f8a299988
comlekahmetcan <@comlekahmetcan.norepyl.github.com>
2022-10-20 10:01:42 +0300

    CA-SS- Reactive kullanımı

--- 10-0-script-setup/src/App.vue
+++ 10-0-script-setup/src/App.vue
@@ -4,11 +4,32 @@
   <button @click="inc">{{ counter }}</button>
   <hr />
   <oddOrEven :counter="counter" @odd-event="alertMe" /><!--ilk gönderme buradan counter için-->
+
+  <hr />
+
+  <h1>User App</h1>
+  <input type="text" v-model="state.personal.name" />
+  <input type="text" v-model="state.personal.lname" />
+  {{ state.personal }}
 </template>
 <script setup>
-//! beforeCreate() created() ve data()
+import { ref, reactive } from "vue";
+//! ---------beforeCreate() created() ve data()
 import oddOrEven from "./components/oddOrEven.vue";
 import Utils from "./composables/Utils.js";
-
 const { title, counter, inc, alertMe } = Utils();
+//! ---------UserApp
+// const name = ref("");          x
+// const lname = ref(" ");        x
+//reactive data(){} vb
+const state = reactive({
+  personal: {
+    name: "AhmetCan",
+    lname: null,
+  },
+  itemList: [],
+});
+// const itemList = ref([]);
+
+console.log("state", state.personal.name);
 </script>
