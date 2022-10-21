8d2be32a4f401123278cdd6d55b0d16504817a11
comlekahmetcan <@comlekahmetcan.norepyl.github.com>
2022-10-21 11:11:00 +0300

    Namespaced Componentler

---
+++ namespaced-components/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+// import MyInput from "./components/Forms/MyInput.vue";
+// import MySelect from "./components/Forms/MySelect.vue";
+// import MyLabel from "./components/Forms/MyLabel.vue";
+import * as Form from "./components/form-components.js";
+</script>
+
+<template>
+  <!-- <MyLabel>
+    Başlık
+    <MyInput />
+  </MyLabel>
+  <MyLabel>
+    Kategori
+    <MySelect />
+  </MyLabel> -->
+
+  <Form.MyLabel>
+    Başlık
+    <Form.MyInput />
+  </Form.MyLabel>
+  <Form.MyLabel>
+    Kategori
+    <Form.MySelect />
+  </Form.MyLabel>
+</template>
---
+++ namespaced-components/src/components/form-components.js
@@ -0,0 +1,3 @@
+export { default as MyInput } from "./Forms/MyInput.vue";
+export { default as MySelect } from "./Forms/MySelect.vue";
+export { default as MyLabel } from "./Forms/MyLabel.vue";
