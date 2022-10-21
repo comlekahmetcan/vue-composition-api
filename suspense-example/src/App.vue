c526064804e22c9feec02b0f0314abcade6b189c
comlekahmetcan <@comlekahmetcan.norepyl.github.com>
2022-10-21 10:29:06 +0300

    Suspense kullanımı

--- suspense-example/src/App.vue
+++ suspense-example/src/App.vue
@@ -1,9 +1,17 @@
 <script setup>
-import MyTodos from "./components/MyTodos.vue";
-import MyUsers from "./components/MyUsers.vue";
+import { defineAsyncComponent } from "@vue/runtime-core";
+// import MyTodos from "./components/MyTodos.vue";
+const MyTodos = defineAsyncComponent(() => import("./components/MyTodos.vue"));
+// import MyUsers from "./components/MyUsers.vue";
 </script>
 <template>
-  <MyUsers />
-  <hr />
-  <MyTodos />
+  <suspense>
+    <!-- asenkron istekler barındırıyor, işlemler bitince bunları ekrana göster -->
+    <template #default>
+      <MyTodos />
+    </template>
+    <template #fallback>
+      <div>Loading...</div>
+    </template>
+  </suspense>
 </template>
