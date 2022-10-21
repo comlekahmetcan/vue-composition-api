82672acd7c40974d4e138ce85cae82ef6aca5d62
comlekahmetcan <@comlekahmetcan.norepyl.github.com>
2022-10-21 10:38:22 +0300

    onErrorCaptured birden fazla component

--- suspense-example/src/App.vue
+++ suspense-example/src/App.vue
@@ -1,25 +1,31 @@
 <script setup>
 import { ref, onErrorCaptured, defineAsyncComponent } from "@vue/runtime-core";
 // import MyTodos from "./components/MyTodos.vue";
+// import MyUsers from "./components/MyUsers.vue";
 const err = ref(null);
 const MyTodos = defineAsyncComponent(() => import("./components/MyTodos.vue"));
+const MyUsers = defineAsyncComponent(() => import("./components/MyUsers.vue"));
 onErrorCaptured((e) => {
   err.value = e;
   return true;
 });
-// import MyUsers from "./components/MyUsers.vue";
 </script>
 <template>
   <span v-if="err" class="error">{{ err }}</span>
   <suspense v-else>
     <!-- asenkron istekler barındırıyor, işlemler bitince bunları ekrana göster -->
     <template #default>
-      <MyTodos />
+      <div>
+        <MyUsers />
+        <hr />
+        <MyTodos />
+      </div>
     </template>
     <template #fallback>
       <div>Loading...</div>
     </template>
   </suspense>
+  <!-- birden fazla suspense yazılabilir -->
 </template>
 
 <style scoped>
