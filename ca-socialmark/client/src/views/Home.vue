d0ea5df1231bab44f1cb17cd5cd4b5ca265a0b26
comlekahmetcan <@comlekahmetcan.norepyl.github.com>
2022-10-24 10:53:09 +0300

    Vuex için useStore kullanımı

--- ca-socialmark/client/src/views/Home.vue
+++ ca-socialmark/client/src/views/Home.vue
@@ -1,17 +1,19 @@
 <template>
   <AppHeader />
   <div class="flex flex-row">
-    <Sidebar @category-changed="updateBookmarkList" />
+    <!-- <Sidebar @category-changed="updateBookmarkList" /> -->
     <app-bookmark-list v-if="bookmarkList.length > 0" :items="bookmarkList" />
     <div v-else>Bookmark bulunmamaktadır..</div>
   </div>
 </template>
 <script setup>
-import Sidebar from "@/components/Home/Sidebar";
+// import Sidebar from "@/components/Home/Sidebar";
 import { ref, onMounted, inject } from "vue";
+import { useStore } from "vuex";
 const appAxios = inject("appAxios");
 const socket = inject("socket");
 const bookmarkList = ref([]);
+const store = useStore();
 
 onMounted(() => {
   socket.on("NEW_BOOKMARK_ADDED", (bookmark) => {
@@ -24,20 +26,20 @@ const fetchData = () => {
   });
   //! Bookmark olarak eklediklerimizi çekmek için user_bookmarks üzerinden çekelim..
   appAxios.get("/user_bookmarks/?_expand=bookmark&_expand=user").then((user_bookmark_response) => {
-    this.$store.commit("setBookmarks", user_bookmark_response?.data);
+    store.commit("setBookmarks", user_bookmark_response?.data);
   });
 
   //! Like olarak eklediklerimizi çekmek için user_likes üzerinden çekelim..
   appAxios.get("/user_likes/?_expand=bookmark&_expand=user").then((user_likes_response) => {
-    this.$store.commit("setLikes", user_likes_response?.data);
+    store.commit("setLikes", user_likes_response?.data);
   });
 };
-const updateBookmarkList = (categoryId) => {
-  const url = categoryId ? `/bookmarks?_expand=category&_expand=user&categoryId=${categoryId}` : `/bookmarks?_expand=category&_expand=user`;
-  appAxios.get(url).then((bookmark_list_response) => {
-    bookmarkList.value = bookmark_list_response?.data || [];
-  });
-};
+// const updateBookmarkList = (categoryId) => {
+//   const url = categoryId ? `/bookmarks?_expand=category&_expand=user&categoryId=${categoryId}` : `/bookmarks?_expand=category&_expand=user`;
+//   appAxios.get(url).then((bookmark_list_response) => {
+//     bookmarkList.value = bookmark_list_response?.data || [];
+//   });
+// };
 
 fetchData();
 </script>
